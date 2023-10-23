d577089a9a32c4dcf07fba84441f6acb318fbdb0
Alarick Whitted <awhitted834@gmail.com>
2023-10-22 23:09:52 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,84 @@
-// Write your solution here!
+const cats=["Milo", "Otis", "Garfield"
+]
+
+function destructivelyAppendCat(name){
+cats.push(name)
+}
+
+
+
+
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+
+
+function destructivelyRemoveLastCat(){
+cats.pop()
+}
+
+
+
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name)
+
+
+}
+
+
+function appendCat(name){
+   const copyOfCats=[...cats]
+   copyOfCats.push(name)
+   return copyOfCats
+
+
+}
+
+function prependCat(name){
+    const daysOfCats=[...cats]
+    daysOfCats.unshift(name)
+    return daysOfCats
+}
+  
+function removeLastCat(){
+
+}
+
+function removeFirstCat(){
+    const daysOfCats=[...cats]
+    daysOfCats.shift(name)
+    return daysOfCats
+} 
+
+function removeLastCat(){
+    const daysOfCats=[...cats]
+    daysOfCats.pop(name)
+    return daysOfCats
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
